4c9fd88bbe9a1626b65b0f358f35a60e9d92349d
luigi_dannibale <luigi.dannibale02@gmail.com>
2022-04-10 19:42:22 +0200

    Speedtest update 1.0

--- Applications/speedtester/speedtest.py
+++ Applications/speedtester/speedtest.py
@@ -13,17 +13,17 @@ Times_speedtest_is_performed = 2
 ###
 
 
-def shift(numero,n):
-  return round((numero / (10**n)),2)
+def shift(num,n):
+  return round((num / (10**n)),2)
 
 def tester():
-  data,ora = tuple(datetime.now().strftime("%b-%d-%Y,%H:%M:%S").split(","))
-  data = data + " " + ora
+  date,hour = tuple(datetime.now().strftime("%b-%d-%Y,%H:%M:%S").split(","))
+  date = date + " " + hour
   s = speedtest.Speedtest()
   upload = shift(s.upload(),6)
   download = shift(s.download(),6)
   ping = s.results.dict()["ping"]
-  text = {"id": data ,"upload-speed": upload,"download-speed": download,"ping-latency": ping}
+  text = {"id": date ,"upload-speed": upload,"download-speed": download,"ping-latency": ping}
   with open(Database_json,"a") as f:
     json.dump(text,f)
   with open(Database_txt,"a") as f:
